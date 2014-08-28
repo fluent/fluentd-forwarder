ac446239fbdaf43557b7bf9a87c7b4235c74ef91
Moriyoshi Koizumi <mozo@mozo.jp>
2014-08-29 03:24:08 +0900

    ???

--- worker_set.go
+++ worker_set.go
@@ -10,21 +10,21 @@ type WorkerSet struct {
 }
 
 func (set *WorkerSet) Add(worker Worker) {
-	defer set.mtx.Unlock()
 	set.mtx.Lock()
+	defer set.mtx.Unlock()
 	set.workers[worker] = struct{}{}
 }
 
 func (set *WorkerSet) Remove(worker Worker) {
-	defer set.mtx.Unlock()
 	set.mtx.Lock()
+	defer set.mtx.Unlock()
 	delete(set.workers, worker)
 }
 
 func (set *WorkerSet) Slice() []Worker {
 	retval := make([]Worker, len(set.workers))
-	defer set.mtx.Unlock()
 	set.mtx.Lock()
+	defer set.mtx.Unlock()
 	i := 0
 	for worker, _ := range set.workers {
 		retval[i] = worker
